0e12252a024a9db3b4a6830240ae590a450afdeb
Vladimir Berdnikov <gazetter@mail.ru>
2021-04-18 18:44:51 +0700

    27.3 добавлены классы для работы с пользователем и утентификации

---
+++ SocialNetwork/BLL/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork.BLL.Exceptions
+{
+    public class UserNotFoundException : Exception 
+    { 
+
+    }
+}
---
+++ SocialNetwork/BLL/Exceptions/WrongPasswordException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork.BLL.Exceptions
+{
+    public class WrongPasswordException : Exception
+    {
+    }
+}
